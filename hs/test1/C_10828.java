432e193ae23ceb5f6754f427629f11912c1bf5c1
시오 <57451700+chunghye98@users.noreply.github.com>
2023-01-25 20:16:54 +0900

    study: Solve 10828, 스택

---
+++ hs/test1/C_10828.java
@@ -0,0 +1,50 @@
+package test1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+// 스택
+public class C_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < test; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push":
+                    int value = Integer.parseInt(st.nextToken());
+                    stack.push(value);
+                    break;
+                case "top":
+                    if (stack.size() == 0) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.size() == 0) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "pop":
+                    if (stack.size() == 0) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+            }
+        }
+    }
+}
