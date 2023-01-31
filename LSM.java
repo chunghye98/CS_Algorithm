468216e9f66e7fcb4ee517ba14f977f7cecffd32
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-01-31 20:38:52 +0900

    study: Solve 11726

--- LSM.java
+++ LSM.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class LSM {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class LSM {
+
+    static Integer GCD(int a, int b){
+        int gcd = b;
+        if (a % b != 0){
+            gcd = GCD(b, a % b);
+        }
+
+        return gcd;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append((a * b) / GCD(a, b) + "\n");
+        }
+
+        System.out.print(sb);
+    }
+
 }
