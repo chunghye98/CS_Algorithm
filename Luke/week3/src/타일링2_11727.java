129fa6c53647b53372a633f1b808f8182da24017
acceptor-gyu <gseonggyu968@gmail.com>
2023-02-08 13:19:16 +0900

    chore: Write description about Dynamic Programming problem

--- Luke/week3/src/타일링2_11727.java
+++ Luke/week3/src/타일링2_11727.java
@@ -2,16 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 문제 조건
- * 시간 제한 1초
- * 주어지는 수 < 11
- *
- * solve1. 1부터 주어진 수까지 for문으로 나누어지는 수가 있는지 검사
- * 시간복잡도: O(N^2) / 최악의 경우: 약 1,000(가장 큰 소수) * 100 = 100,000
- * -> 해당 문제에서는 O(N^2)으로 풀어도 시간 제한에 걸리지 않을 것이다.
- */
-
 public class 타일링2_11727 {
 
     public static int[] dp;
