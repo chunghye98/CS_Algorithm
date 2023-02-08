35d0af42badb6d1734f6de6a62e44353f95be5e2
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-02-08 15:27:07 +0900

    study Solve: w3 algorithms

--- Movie/W2/DPtiles.java
+++ Movie/W2/DPtiles.java
@@ -1,4 +1,4 @@
-package Movie;
+package Movie.W2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
---
+++ Movie/W3/PushPop.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+
+public class PushPop {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int numbers = 1;
+
+        Stack<Integer> Sequence = new Stack<>();
+        Sequence.push(0);
+
+        Boolean possible = true;
+
+        for(int i = 1; i <= T; i++){
+            if (possible) {
+                int n = Integer.parseInt(br.readLine());
+                while((n != Sequence.peek()) && numbers <= n){
+                    Sequence.push(numbers);
+                    numbers++;
+                    sb.append("+\n");
+                }
+                if (n !=Sequence.peek()){
+                    possible = false;
+                }else {
+                    Sequence.pop();
+                    sb.append("-\n");
+                }
+            }
+
+        }
+
+        if (possible){
+            System.out.print(sb);
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
