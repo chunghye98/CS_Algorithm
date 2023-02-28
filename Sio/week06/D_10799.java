9fda7987c2feba9ebbe9b88456ca39352a2fe3ed
chung <chunghye1998>
2023-03-01 01:39:07 +0900

    study: 10799 인터넷 코드 보고 풀었음

--- Sio/week06/D_10799.java
+++ Sio/week06/D_10799.java
@@ -1,48 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
 import java.util.Stack;
 
 /* 쇠막대기
 - 시간 제한 : 1초
 - 입력: 최대 10만
-
+- 3일동안 붙잡았지만 틀렸습니다 나옴
+- 안풀려서 숨넘어감
+- 인터넷 코드 이해
+- 가로가 아닌 세로로 생각해야 할 줄은 몰랐지
+- ( 이면 stack에 push
+- ) 이면 두 가지 경우 생각
+    - 직전이 ( 인가 -> 레이저이므로 stack.pop 해서 레이저 없애버리고 stack.size를 result에 누적
+    - 직전이 ) 이면 -> 레이저 아니고 그냥 막대니까 result +1
  */
 public class D_10799 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine().replace("()", "*");
-
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = bf.readLine();
         Stack<Character> stack = new Stack<>();
-        int inputIndex = 0;
-        int lazerCount = 0;
-        int wholeLazerCount = 0;
-        int rodCount = 0; // 쇠막대기 개수
-        boolean flag = false;
-        while (inputIndex < input.length()) {
-            char c = input.charAt(inputIndex);
-            if (c == '(') {
-                flag = false;
-                stack.push(c);
-                wholeLazerCount += lazerCount;
-                lazerCount = 0;
-            } else if (c == '*' && !stack.isEmpty()) {
-                lazerCount++;
-            } else if (c == ')') {
-                stack.pop();
-                wholeLazerCount += lazerCount;
-                if (flag) {
-                    rodCount += wholeLazerCount + 1;
-                } else {
-                    rodCount += lazerCount + 1;
-                }
-                lazerCount = 0;
-                flag = true;
+
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') { // 열린 괄호면 스택에 추가.
+                stack.push('(');
+                continue;
             }
-            inputIndex++;
+            if (input.charAt(i) == ')') { // 닫힌 괄호일 경우,
+                stack.pop(); // 일단 stack에서 pop을 실행.
+
+                if (input.charAt(i - 1) == '(') { // 그 전 괄호가 열린 괄호면 레이저를 의미.
+                    result += stack.size(); // 현재 stack의 사이즈만큼 더해 줌.
+                } else { // 그 전 괄호가 닫힌 괄호면 레이저가 아님.
+                    result++; // 단순히 1을 더해 줌.
+                }
+            }
         }
-        System.out.println(rodCount);
+
+        bw.write(result + "\n");
+        bw.flush();
+        bf.close();
+        bw.close();
     }
 }
