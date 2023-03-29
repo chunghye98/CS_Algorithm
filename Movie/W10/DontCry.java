bfc7a2be74c6789a6ecff6c0415de51694a9334e
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-29 23:02:12 +0900

    chore: add 안하고 커밋해서 다시 합니다~

--- Movie/W10/DontCry.java
+++ Movie/W10/DontCry.java
@@ -1,2 +1,40 @@
-package W10;public class DontCry {
+package W10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class DontCry{
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            if (Integer.parseInt(st.nextToken()) == 0) {
+                if (queue.isEmpty()) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(queue.poll() + "\n");
+                }
+            } else {
+                while (st.hasMoreTokens()) {
+                    queue.add(Integer.parseInt(st.nextToken()));
+                }
+            }
+
+        }
+
+        System.out.println(sb);
+
+    }
+
+
 }
--- Movie/W10/Truck.java
+++ Movie/W10/Truck.java
@@ -1,2 +1,60 @@
-package W10;public class Truck {
+package W10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Truck {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> trucks = new ArrayDeque<>();
+
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            trucks.addLast(Integer.parseInt(st.nextToken()));
+        }
+
+        Deque<Integer> bridge = new ArrayDeque<>();
+        for (int i = 0; i < W; i++) {
+            bridge.addLast(0);
+        }
+
+        int bridgeWeight = 0;
+
+        Deque<Integer> finished = new ArrayDeque<>();
+        int time = 0;
+
+        while (finished.size() != N) {
+            time++;
+
+            if (bridge.peekFirst() != 0) {
+                bridgeWeight -= bridge.peekFirst();
+                finished.add(bridge.pollFirst());
+            } else {
+                bridge.pollFirst();
+            }
+
+            if (!trucks.isEmpty() && bridgeWeight + trucks.peekFirst() <= L) {
+                bridgeWeight += trucks.peekFirst();
+                bridge.addLast(trucks.pollFirst());
+            } else {
+                bridge.addLast(0);
+            }
+
+        }
+
+        System.out.println(time);
+
+
+    }
 }
