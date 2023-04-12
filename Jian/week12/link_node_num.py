c25a5759b7a7ce6105645975005c436344d44594
sudago <sudago22@gmail.com>
2023-04-12 23:45:29 +0900

    fix: Solve 11724번, 연결 요소의 개수 bfs

--- Jian/week12/link_node_num.py
+++ Jian/week12/link_node_num.py
@@ -1,14 +1,19 @@
 # 11724번, 연결 요소의 개수
 
 import sys
+from collections import deque
 input_line = sys.stdin.readline
 
 
-def dfs(ver):  # vertex 정점
+def bfs(ver):  # vertex 정점
+    queue = deque([ver])
     visited[ver] = True
-    for node in graph[ver]:
-        if not visited[node]:
-            dfs(node)
+    while queue:
+        node = queue.popleft()
+        for linked_node in graph[node]:
+            if not visited[linked_node]:
+                visited[linked_node] = True
+                queue.append(linked_node)
 
 
 n, m = map(int, input_line().rstrip().split())
@@ -21,11 +26,11 @@ for _ in range(m):  # graph 양방향 연결
     graph[u].append(v)
     graph[v].append(u)
 
-dfs(1)
+bfs(1)
 
 for i in range(1, n+1):
     if not visited[i]:
-        dfs(i)
+        bfs(i)
         count += 1
 
 print(count)
