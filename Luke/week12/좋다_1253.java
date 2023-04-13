557e165def8fcb5613567cddb4d289fe65ed6db2
acceptor-gyu <gseonggyu968@gmail.com>
2023-04-13 21:54:10 +0900

    study: [acmicpc.net 1253] two pointer
    해시맵으로 시도했지만 예외처리가 너무 많아져서 계속 틀렸다.
    처음부터 투 포인터를 생각했지만
    투 포인터를 사용하지 않고 하려다가 자주 틀려서 투 포인터로 했다...

--- Luke/week12/좋다_1253.java
+++ Luke/week12/좋다_1253.java
@@ -7,6 +7,20 @@ import java.util.StringTokenizer;
 /**
  * 시간 제한: 2초
  * -> O(N^2 log N)까지도 가능할 것같다!
+ *
+ * 이전 시도의 문제점
+ * - 1 1 2인 경우 binarySearch(numbers, 1)을 하면 둘 다 0을 반환한다.
+ * - 1 2 인 경우도 cnt가 증가한다.
+ *
+ * binarySearch의 결과!
+ * -> 같은 숫자가 2개 들어있으면 앞의 인덱스를 반환하는지 뒤의 인덱스를 반환하는지 보장하지 못한다.
+ *
+ * 개수를 나타내는 1 <= N <= 2000을 각 숫자의 범위로 생각하고 풀었다....
+ * 조건 확인 잘하자..
+ *
+ * +
+ * 처음부터 투 포인터 생각했는데.. 투 포인터를 안써도 풀 수 있을 것같았다...
+ * -> 예외처리가 많아짐...
  */
 
 public class 좋다_1253 {
@@ -24,15 +38,36 @@ public class 좋다_1253 {
 
         Arrays.sort(numbers);
 
-        for (int i = N - 1; i > 0; i--) {
+        for (int i = N - 1; i >= 0; i--) {
+            int left = 0;
+            int right = N - 1;
+
             int target = numbers[i];
-            for (int j = 0; j < N; j++) {
-                int smallIdx = Arrays.binarySearch(numbers, numbers[j]);
-                int bigIdx = Arrays.binarySearch(numbers, target - numbers[j]);
-                if (smallIdx >= 0 && bigIdx >= 0) {
-                    cnt++;
+
+            while (true) {
+                if (left == i) {
+                    left++;
+                }
+
+                if (right == i) {
+                    right--;
+                }
+
+                if (left >= right) {
                     break;
                 }
+
+                if (numbers[left] + numbers[right] < target) {
+                    left++;
+                    continue;
+                }
+
+                if (numbers[left] + numbers[right] > target) {
+                    right--;
+                    continue;
+                }
+                cnt++;
+                break;
             }
         }
 
