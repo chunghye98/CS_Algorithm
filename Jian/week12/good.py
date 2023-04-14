b4400ce3837fd7c4ba06c6235ee6a85890e572c8
sudago <sudago22@gmail.com>
2023-04-14 10:10:19 +0900

    fix: Solved 1253번, 좋다
    투포인터 사용

--- Jian/week12/good.py
+++ Jian/week12/good.py
@@ -4,23 +4,21 @@ input_l = sys.stdin.readline
 
 n = int(input_l().rstrip())
 arr = list(map(int, input_l().rstrip().split()))
-count = [0] * (n+1)
 arr.sort()
+result = 0
 
-for i in range(n-1, 0, -1):  # for (int i = n-1; i > 0; i--)과 동일. 비교할 두 수가 필요하므로 index 기준 1까지만 돈다.
+for i in range(n):
     target = arr[i]
-    if (n > i+1) and (target == arr[i+1]):  # 중복 계산 방지. ex) 0, 1, 2, 2 일 때, 앞 순서의 2일 경우 뒷 순서의 2로 GOOD 계산이 이미 되어있다.
-        count[i] = count[i+1]
-    else:  # 이미 계산한 숫자가 중복 숫자가 아닐 때, GOOD 계산을 시작한다.
-        for j in range(i-1, 0, -1):  # 뒤부터 비교한다.
-            max_n = arr[j]  # ex) 1, 2, 3, 4 일 때, target = 4, max_n = 3, min_n = 1
-            for z in range(j):
-                min_n = arr[z]
-                if (min_n + max_n) == target:  # ex) 1 + 3 == 4 이면
-                    count[i] = 1  # 두 수의 합으로 나타낼 수 있어서 GOOD이므로, 해당 index에 1을 count 해준다.
-                    break  # 더 이상 GOOD 인지 확인 할 필요 없으므로 3중 for문 탈출
-            if count[i] == 1:  # target이 GOOD인지 확인 했으면
-                break  # 2중 for문 탈출
+    tmp = arr[:i] + arr[i+1:]  # arr[i]를 제외한 나머지 요소들이 들어있는 배열. 동일한 숫자가 나올 수 있으므로 정렬되어 있더라도 타켓 index 이후의 요소도 배열에 추가한다.
+    left, right = 0, len(tmp)-1
+    while left < right:
+        if target == (tmp[left] + tmp[right]):  # 타켓이 두 수의 합과 동일하다면
+            result += 1
+            break
 
-print(sum(count))  # 두 수로 나타낼 수 있는 모든 GOOD을 카운트 한다.
+        if target > (tmp[left] + tmp[right]):  # 두 수의 합이 타켓보다 작다면
+            left += 1
+        else:  # 두 수의 합이 타켓보다 크다면
+            right -= 1
 
+print(result)
