23ac94815a0f9f6b885a606f3126aee18ec339a1
sudago <sudago22@gmail.com>
2023-04-20 00:39:02 +0900

    study: unsolved 2531번, 회전 초밥
    list와 set을 이용하여 제출, 시간 초과

--- Jian/week13/sushi.py
+++ Jian/week13/sushi.py
@@ -1,25 +1,22 @@
 # 2531번, 회전 초밥
 import sys
-import itertools
-from collections import deque
 input_line = sys.stdin.readline
 
 
 N, d, k, c = map(int, input_line().rstrip().split())
-sushi = deque()
+sushi = [int(input_line().rstrip()) for _ in range(N)]
 result = 0
 
-for _ in range(N):  # 초밥 넣기
-    sushi.append(int(input_line().rstrip()))
+for i in range(N):  # 연속해서 먹는 가짓 수(k) 판별하기
+    eat = []
+    for j in range(i, i+k):  # k 개가 들어가게 만든다.
+        eat.append(sushi[j % N])  # 배열을 넘어가도 다시 index 0부터 넣어준다.
 
-for _ in range(N):  # 연속해서 먹는 가짓 수(k) 판별하기
-    eat = list(itertools.islice(sushi, 0, k))
     if len(eat) == len(set(eat)):
         if c in eat:  # 쿠폰 번호가 현재 배열에 있으면
             result = k
         else:  # 쿠폰 번호가 현재 배열에 없으면
             result = k+1
-            break
-    sushi.append(sushi.popleft())  # 판별하지 못한 초밥은 계속 뒤에 넣어준다
+            break  # 최대값을 찾았으니 바로 종료
 
 print(result)
