2eee858471c7634720d6695da5235855acb73b1b
sudago <sudago22@gmail.com>
2023-04-25 12:58:07 +0900

    fix: week13 수정
    2493번, 탑 
    16234번, 인구 이동

--- Jian/week13/people_move.py
+++ Jian/week13/people_move.py
@@ -4,55 +4,47 @@ from collections import deque
 
 input_line = sys.stdin.readline
 
-
-def gate_open(county, x, y):
-    row = [0, 0, -1, 1]  # 상 하 좌 우
-    column = [1, -1, 0, 0]
-    flag = False  # default 는 연합 불가능한 나라라고 가정.
-    for i in range(4):
-        check_x = x + row[i]
-        check_y = y + column[i]
-        if 0 <= check_x < N and 0 <= check_y < N:  # 비교 할 나라가 index가 범위 내에 있고
-            population = county[x][y] - county[check_x][check_y]  # 인구 차이가
-            if L <= population <= R or -R <= population <= -L:  # (20 < 인구 차이 < 50) 혹은 (-50 < 인구 차이 < -20) 이라면
-                graph[x][y].append([check_x, check_y])  # 쌍방향 연결을 하고
-                graph[check_x][check_y].append([x, y])
-                flag = True  # 국경이 열릴 수 있는, 연합 가능한 나라다. (인구 이동 가능)
-    return flag
-
-
-def bfs(start):
-    queue = deque(graph[start])
-    tmp = 0
-    connect_country = 0
-    while queue:
-        x, y = queue.popleft()
-        for another_node in graph[node]:  # graph[1] 부터 연결된 좌표 탐색
-            x, y = another_node
-            if not visited[x][y]:  # 방문 했는지 판별 (같은 국가의 인구 수를 또 넣을 수도 있으니까)
-                visited[x][y] = True  # 방문 표시
-                tmp += country[x][y]  # 인구 수 더하기
-                connect_country += 1  # 연합 한 국가의 수 세기
-
-
 N, L, R = map(int, input_line().rstrip().split())
-country = [list(map(int, input_line().rstrip().split())) for _ in range(N)]
-graph = [[] * N for _ in range(N)]
+graph = [list(map(int, input_line().rstrip().split())) for _ in range(N)]
 day = 0
 
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(a, b):
+    q = deque()
+    temp = []
+    q.append((a, b))
+    temp.append((a, b))
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+        if (0 <= nx <= N and 0 <= ny <= N) and (L <= abs(graph[nx][ny]) <= R) and not visited[nx][ny]:
+            visited[nx][ny] = True
+            q.append([nx, ny])
+            temp.append([nx, ny])
+    return temp
+
+
 while True:  # 연합 할 나라가 없으면 종료
-    close_cnt = 0
+    visited = [[False] * (N + 1) for _ in range(N + 1)]
+    flag = False
     for i in range(N):
         for j in range(N):
-            if not gate_open(country, i, j):  # 국경 선을 열 수 있는 나라면 (인구 이동 가능) 쌍방향 연결까지.
-                close_cnt += 1  # 국경 선을 못연다면 카운트
+            if not visited[i][j]:
+                visited[i][j] = True
+                country = bfs(i, j)  # 연합 할 나라의 좌표값이 담겨있다.
+                if len(country) > 1:  # 연합 할 나라가 하나 이상이면
+                    flag = True
+                    number = sum([graph[x][y] for x, y in country]) // len(country)  # 인구 이동이 이루어 진다.
+                    for x, y in country:
+                        graph[x][y] = number  # 평균 인구를 연합 한 나라에 배분한다.
 
-    if close_cnt == N * N:  # 연합 할 나라가 없으면 종료
+    if not flag:  # 연합 할 나라가 없으면 국경선을 닫는다.
         break
-
-    visited = [[False] * N for _ in range(N)]
-    for i in range(N):
-        bfs(i)
-    day += 1  # 인구 이동 며칠 했는지 count
+    day += 1  # 하루가 지난다.
 
 print(day)
--- Jian/week13/tower.py
+++ Jian/week13/tower.py
@@ -3,22 +3,22 @@ from collections import deque
 
 N = int(input())
 tower = deque(list(map(int, input().split())))  # stack으로 사용
-stack = deque()
-result = deque()
+stack = []  # [높이, index+1] 형태로 넣어준다.
+result = [0] * N
 
-for _ in range(N):
+for i in range(N-1, -1, -1):
     while stack:  # stack == 0, tower == 0
-        if tower[-1] > stack[-1]:  # 오른쪽 타워보다 왼쪽 타워가 더 높으면
-            result.append(len(tower))  # 결과에 왼쪽 타워의 index를 넣는다.
+        if tower[-1] > stack[-1][0]:  # 오른쪽 타워보다 왼쪽 타워가 더 높으면
+            result[stack[-1][1]] = (len(tower))  # 결과에 왼쪽 타워의 index를 넣는다.
             stack.pop()  # stack에서 제거
         else:  # stack과 비교 중단.
             break  # while stack: 탈출
-    stack.append(tower.pop())
+    stack.append([tower.pop(), i])
 
-for _ in stack:  # stack에 타워가 남아있으면
-    result.append(0)  # 비교할 타워가 없으므로 0을 넣어준다.
+for i in range(len(stack)):  # stack에 타워가 남아있으면
+    result[stack[i][1]] = 0  # 비교할 타워가 없으므로 0을 넣어준다.
 
-for _ in range(len(result)):
-    print(result.pop(), end=" ")
+for i in range(N):
+    print(result[i], end=" ")
 
 
