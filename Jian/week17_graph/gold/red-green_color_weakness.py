6fcb9121ef673d49d0cc14ec057587f968f2742f
sudago <sudago22@gmail.com>
2023-05-21 19:54:40 +0900

    study: Solve 10026번, 적록색약
    비색약인과 색약인의 그래프를 따로 만들어서 bfs를 실행시켰다.

--- Jian/week17_graph/gold/red-green_color_weakness.py
+++ Jian/week17_graph/gold/red-green_color_weakness.py
@@ -1,16 +1,12 @@
 # 10026번, 적록색약
+# 틀린 이유: R, G만 있으면 하나로 인식해야 함.
+# 반례: 2 "\n" RG "\n" RG
 import sys
+import copy
 from collections import deque
 readl = sys.stdin.readline
 
-dx = [-1, 1, 0, 0]  # 상,하,좌,우
-dy = [0, 0, -1, 1]
-n = int(readl().rstrip())
-graph = [list(readl().rstrip()) for _ in range(n)]  # 색약X
-R, G, B = [0, 0, 0]
-result = [0, 0]  # 색약X, 색약O
-
-def bfs(a, b, color):
+def bfs(a, b, color, graph):
     queue = deque()
     queue.append((a, b))
     while queue:
@@ -24,25 +20,33 @@ def bfs(a, b, color):
                 graph[nx][ny] = "W"  # White(흰색)로 방문 표시
                 queue.append((nx, ny))
 
+def color_change(graph):  # 색약X 그래프 -> 색약O 그래프
+    graph2 = copy.deepcopy(graph)
+    for i in range(n):
+        for j in range(n):
+            if graph2[i][j] == "G":
+                graph2[i][j] = "R"
+    return graph2
+
+dx = [-1, 1, 0, 0]  # 상,하,좌,우
+dy = [0, 0, -1, 1]
+n = int(readl().rstrip())
+graph = [list(readl().rstrip()) for _ in range(n)]  # 색약X
+graph2 = color_change(graph)  # 색약O
+result = [0, 0]  # 색약X, 색약O
+
 # 로직
 for i in range(n):
     for j in range(n):
-        color = graph[i][j]
-        if color == "R":
-            R += 1
-            bfs(i, j, color)
-        if color == "G":
-            G += 1
-            bfs(i, j, color)
-        if color == "B":
-            B += 1
-            bfs(i, j, color)
+        color = graph[i][j]  # 색약X
+        color2 = graph2[i][j]  # 색약O
+        if color != "W":  # 색약X
+            result[0] += 1
+            bfs(i, j, color, graph)
+        if color2 != "W":  # 색약O
+            result[1] += 1
+            bfs(i, j, color2, graph2)
 
-result[0] = R + G + B  # 색약X
-result[1] = max(R, G) + B  # 색약O
-
-print(f"R:{R}")
-print(f"G:{G}")
-print(f"B:{B}")
+# 출력
 for r in result:
     print(r, end=" ")
