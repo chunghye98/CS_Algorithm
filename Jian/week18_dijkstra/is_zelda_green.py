c42ee1da2c2e6b21104a7e20186d56955e2e98bd
sudago <sudago22@gmail.com>
2023-06-02 13:12:26 +0900

    refactor: Solve 4485번, 녹색 옷 입은 애가 젤다지?
    bfs를 dijkstra로 수정했다.
    - 제출 시간이 단축 된 이유:
    (0, 0)부터 (n-1, n-1)로 가는데 방문하지 못하는 노드는 없기 때문에(방해물) 모든 노드를 돌며 방문하지 않은 노드를 탐색하는 2중 for문을 제거했다.
    - deque 대신 priority queue 사용하는 이유
    priority queue(heapq)를 이용해서 min-heap(거리)으로 해당 노드까지의 최단 거리를 먼저 계산해준다.
    이후에 오는 거리는 모두 min-heap 보다 크기 때문에 최단 거리가 아니게 된다.

--- Jian/week18_dijkstra/is_zelda_green.py
+++ Jian/week18_dijkstra/is_zelda_green.py
@@ -1,11 +1,30 @@
 # 4485번, 녹색 옷 입은 애가 젤다지?
 import sys
-from collections import deque
+import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 tc = 1
+
+def dijkstra():
+    q = []
+    distance[0][0] = graph[0][0]
+    # 거리, x좌표, y좌표
+    heapq.heappush(q, (distance[0][0], 0, 0))  # 맨 앞의 값(거리)을 기준으로 정렬
+
+    while q:
+        dist, x, y = heapq.heappop(q)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:  # 범위 초과
+                continue
+            cost = dist + graph[nx][ny]
+            if distance[nx][ny] > cost:  # 최소 거리 갱신해야 한다면
+                distance[nx][ny] = cost
+                heapq.heappush(q, (cost, nx, ny))
+
 
 while True:
     n = int(input().rstrip())
@@ -14,29 +33,6 @@ while True:
     graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
     distance = [[INF] * n for _ in range(n)]
 
-    def bfs(a, b):
-        q = deque()
-        q.append((a, b))
-        if a == 0 and b == 0:  # 최초 한번만 돌게 해준다.
-            distance[0][0] = graph[0][0]
-
-        while q:
-            x, y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx < 0 or nx >= n or ny < 0 or ny >= n:  # 범위 초과
-                    continue
-                cost = graph[nx][ny] + distance[x][y]
-                if distance[nx][ny] > cost:  # 최소 거리 갱신해야 한다면
-                    distance[nx][ny] = cost
-                    q.append((nx, ny))
-
-
-    for i in range(n):
-        for j in range(n):
-            if distance[i][j] == INF:
-                bfs(i, j)
-
+    dijkstra()
     print(f"Problem {tc}: {distance[n-1][n-1]}")
     tc += 1
